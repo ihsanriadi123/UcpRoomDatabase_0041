5cec02bd6531678d94590db0fd4804e152fb613a
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2024-12-21 14:33:13 +0700

    Dokter adalah entitas Room yang merepresentasikan tabel dokter dalam database.

--- app/src/main/java/com/example/ucp2crud/data/entity/Dokter.kt
+++ app/src/main/java/com/example/ucp2crud/data/entity/Dokter.kt
@@ -5,7 +5,8 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "dokter")
 data class Dokter(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
     val nama: String,
     val spesialis: String,
     val klinik: String,
