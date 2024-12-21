cd16490d57d6495f0936ec57258b335f0e0dfe00
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2024-12-21 14:30:26 +0700

    JadwalDao adalah interface Room untuk mengelola operasi CRUD data jadwal secara asinkron dan reaktif.

--- app/src/main/java/com/example/ucp2crud/data/dao/JadwalDao.kt
+++ app/src/main/java/com/example/ucp2crud/data/dao/JadwalDao.kt
@@ -6,24 +6,25 @@ import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 import com.example.ucp2crud.data.entity.Jadwal
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface JadwalDao {
     @Insert
     suspend fun insertJadwal(jadwal: Jadwal)
 
+    @Query("SELECT * FROM jadwal ORDER BY namaPasien ASC")
+    fun getAllJadwal(): Flow<List<Jadwal>>
+
     @Query("SELECT * FROM jadwal WHERE id = :id")
-    suspend fun getJadwalById(id: Int): Jadwal
-
-    @Query("SELECT * FROM jadwal")
-    suspend fun getAllJadwal(): List<Jadwal>
-
-    @Update
-    suspend fun updateJadwal(jadwal: Jadwal)
+    fun getJadwalById(id: Int): Flow<Jadwal>
 
     @Delete
     suspend fun deleteJadwal(jadwal: Jadwal)
 
+    @Update
+    suspend fun updateJadwal(jadwal: Jadwal)
 }
+
 
 
