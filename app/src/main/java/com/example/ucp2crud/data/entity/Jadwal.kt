a54fd80d262ec9a3942fd3e55c5245bacaba1297
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2024-12-21 14:33:54 +0700

    Jadwal adalah entitas Room dengan relasi ke tabel dokter menggunakan foreign key.

--- app/src/main/java/com/example/ucp2crud/data/entity/Jadwal.kt
+++ app/src/main/java/com/example/ucp2crud/data/entity/Jadwal.kt
@@ -1,11 +1,25 @@
 package com.example.ucp2crud.data.entity
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "jadwal")
+
+
+@Entity(tableName = "jadwal",
+    foreignKeys = [
+        ForeignKey(
+            entity = Dokter::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("id"),
+            onUpdate = ForeignKey.CASCADE,
+            onDelete = ForeignKey.CASCADE
+        )]
+)
+
 data class Jadwal(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
     val namaDokter: String,
     val namaPasien: String,
     val noHp: String,
