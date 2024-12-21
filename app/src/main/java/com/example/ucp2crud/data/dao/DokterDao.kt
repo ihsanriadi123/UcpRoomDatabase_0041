bfd0920c39ab0113b18140a869b2ee69a6307dd7
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2024-12-21 14:28:03 +0700

    insertDokter (untuk menyisipkan data dokter) dan getAllDokter (untuk mengambil semua data dokter yang diurutkan) dibuat deskriptif agar jelas dan mudah dipahami sesuai tugasnya.

--- app/src/main/java/com/example/ucp2crud/data/dao/DokterDao.kt
+++ app/src/main/java/com/example/ucp2crud/data/dao/DokterDao.kt
@@ -1,26 +1,19 @@
 package com.example.ucp2crud.data.dao
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
-import androidx.room.Update
 import com.example.ucp2crud.data.entity.Dokter
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface DokterDao {
     @Insert
     suspend fun insertDokter(dokter: Dokter)
 
+    //get All Dokter
     @Query("SELECT * FROM dokter ORDER BY nama ASC")
-    suspend fun getAllDokter(): List<Dokter>
+    fun getAllDokter(): Flow<List<Dokter>>
 
-    @Query("SELECT * FROM dokter WHERE id = :id")
-    suspend fun getDokterById(id: Int): Dokter
-
-    @Delete
-    suspend fun deleteDokter(dokter: Dokter)
-
-    @Update
-    suspend fun updateDokter(dokter: Dokter)
 }
+
